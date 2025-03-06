e72304ab7e6e54487675592a6ce8912909f53b3d
hg <munroeparlee@gmail.com>
2025-03-06 14:20:17 -0800

    declared sensor read value functions, defined get_sensors_data function

--- project/Core/Src/obc.c
+++ project/Core/Src/obc.c
@@ -1,14 +1,18 @@
 #include "obc.h"
 
-void init_sensors(){
-    SensorsData 
-}
+
 float read_temperature();
 float read_pressure();
-float read_gyroscope();
-/*
-SensorsData read_sensors(){
-    sensors_data.temperature = read_temperature();
-    sensors_data.pressure = read_pressure();
-    sensors_data.gyroscope = read_gyroscope();
-*/
+float read_gyroscope_x1();
+float read_gyroscope_x2();
+float read_gyroscope_x3();
+
+SensorsData get_sensors_data(){
+    SensorsData data;
+    data.temperature = read_temperature();
+    data.pressure = read_pressure();
+    data.gyroscope_axis_1 = read_gyroscope_x1();
+    data.gyroscope_axis_2 = read_gyroscope_x2();
+    data.gyroscope_axis_3 = read_gyroscope_x3();
+    return data;
+}
