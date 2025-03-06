161e62f0b0b68ad0ea038639aafeefd48bf3925b
Zoë van de Vegte <156561301+zvdv@users.noreply.github.com>
2025-03-06 12:02:31 +0600

    Create empty functions

---
+++ project/Core/Inc/obc_interface.h
@@ -0,0 +1,3 @@
+// Functions for storing to SD card
+void store_data(/*arguments*/);
+void store_image(/*arguments*/);
--- project/Core/Src/obc_interface.c
+++ project/Core/Src/obc_interface.c
@@ -1,1 +1,11 @@
 #include "obc_interface.h"
+
+// Function to store telemetry/errors/etc on SD card
+void store_data(/*arguments*/){
+
+}
+
+// Function to store images on SD card
+void store_image(/*arguments*/){
+
+}
