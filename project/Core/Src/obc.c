91c5a7e072908b0b55a3606440e5b7befca95e2e
Ethanium314 <ethanjamesdean@gmail.com>
2025-03-18 20:05:01 -0700

    Adds task for data collection

--- project/Core/Src/obc.c
+++ project/Core/Src/obc.c
@@ -9,7 +9,26 @@ void obc_notifications(void *vpParameters) {
         received_notification = ulTaskNotifyTake(pdFALSE, 0);
 
         if (received_notification & REQUEST_CAMERA) {
-            // TODO
+            take_snapshot(/* TODO: Create and add camera configurations */);
+
+            // TODO: Send confirmation notification
         }
     }
+}
+
+data_task(void *vpParamters) {
+	float voltage;
+	float current;
+	float temperature;
+	// TODO: Add other data points
+	while (1) {
+		voltage = read_battery_voltage();
+		current = read_battery_current();
+		temperature = read_battery_temperature();
+		// TODO: Read other sensor data
+
+		// TODO: Send data to TTC
+
+		// TODO: Store data in memory
+	}
 }
