702595f4d4a02a08faaf06e4e1e9400f10998cb6
avabirtwistle <148493409+avabirtwistle@users.noreply.github.com>
2025-03-20 11:29:50 -0700

    Update ttc_interface.c
    added a method to send command to the transceiver, added method to transmit the buffer contents, added command to load the buffer

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -1,6 +1,4 @@
 #include "ttc_interface.h"
-#include "cc120x_spi.h"
-#include "cc120x_spi.c"
 
 //===============================================================================
 //============================**TRANSMIT**===============================
@@ -23,100 +21,44 @@
 
 //CC12x FIFO Transmit Registers
 #define CC12_BURST_TRANS                  0x007F //burst transmission
-HAL_StatusTypeDef status;
+#define CC12_TRANS_START                  0x35 //starts transmission
 
 //Acknowledgement Response
 uint8_t ack = 0; 
 //===============================================================================
-typedef struct {
-  uint8_t *data; //pointer to the dynamically allocated data buffer
-  uint8_t length; //length of the data in bytes
-  uint8_t retries; //number of retransmissions for this packet
-  bool ack_req; //if this data type requires an acknowledgement, set by data packager
-}TxPayload_t;
 
+void transmit(){
+  //send command to transmit the buffer contents
+  CC12_SendCommand(CC12_TRANS_START);
+}
 
-//===============================================================================
-/*
- *@brief task responsible for handling SPI transmission
-* Task waits for transmission data from the RTOS queue
-* Queue holds pointers to data structure that is dynamically allocated before queuing
-* This task is responsible for freeing the memory it receives ownership of after transmission
-* Task is consumer
-*/
-void vTransmitTask(void *pvParameters){
-  TxPayload_t *txBuffer; // pointer who holds received queue data
-  rfStatus_t status;
+void writeToTransmitBuffer(uint8_t *data){
+  //pull cs low to start spi communication
+  HAL_GPIO_WritePin(CC12_CSn_GPIO, CC12_CSn_PIN, GPIO_PIN_RESET);
+
+  //wait for MISO to go low
+  while(HAL_GPIO_ReadPin(CC12_SPI_GPIO,CC12_SO_PIN));
+
+  //send the burst command
+  CC12_SendCommand(CC12_BURST_TRANS);
+
+  HAL_SPI_Transmit(&hspi1, data, length, HAL_MAX_DELAY);
   
-  while(1){
-    //wait for data to be placed on queue
-    if(xQueueReceive(txQueue, &txBuffer, portMAX_DELAY) == pdPASS){
-      status = transmit(txBuffer)
-      handle_tx_memory(txBuffer);
-      }
-  }
+  //pull CS high to end SPI communication
+  HAL_GPIO_WritePin(CC12_CSn_GPIO, CC12_CSn_PIN, GPIO_PIN_SET);
 }
 
-//transmits the data and sends specific error codes to allow for better handling/more customizable behaviot
-tx_status_t transmit(TxPayload_t *txBuffer){
-  rfStatus_t status;
 
-  if(txBuffer == NULL || txBuffer->data == NULL){
-    return TX_ERROR_NULL_BUFFER;
-  }
+void CC12_SendCommand(uint8_t command){
+  //pull cs low to start spi communication
+  HAL_GPIO_WritePin(CC12_CSn_GPIO, CC12_CSn_PIN, GPIO_PIN_RESET);
 
-  //retry transimission up to txBuffer-> retries
-  for(uint8_t attempt = 0; attempt <= txBuffer->retries; attempt++){
-    //check for space on the buffer
-    if((cc120xGetTxStatus() & 0xF0) == 0xF0){
-      
-          // wait for FIFO to clear or handle a timeout        
-          //Handle timeout: reset FIFO or return error 
-    }
-    //write the length first
-    status = cc120xSpiWriteTxFifo(&txBuffer->length, 1);//the size of the packet should always be just one byte
-      if (status == 0xF0) // FIFO Full
-      {
-          cc120xSpiCmdStrobe(CC120X_SFTX); // Flush TX FIFO
-          continue; // Retry
-      }
-      else if (status == 0xFF) // SPI Failure
-      {
-          return TX_ERROR_SPI_FAILURE;
-      }
-    //write data payload
-    status = cc120xSpiWriteTxFifo(txBuffer->data, txBuffer->length);//
-      if (status == 0xF0) //FIFO Full
-      {
-          cc120xSpiCmdStrobe(CC120X_SFTX); // Flush TX FIFO
-          continue; // Retry
-      }
+  //wait for MISO to go low
+  while(HAL_GPIO_ReadPin(CC12_SPI_GPIO,CC12_SO_PIN));
 
-      //check for SPI Failure
-      if (status == 0xFF){
-          
-          continue; // Retry
-      }
+  //transmit command over spi
+  HAL_SPI_Transmit(&hspi1, &command, 1, HAL_MAX_DELAY);
 
-    // trigger transmission: send TX Strobe command
-    status = cc120xSpiCmdStrobe(CC120X_STX);
-    // wait for transmission to complete
-    while((cc120xGetTxStatus() & 0x70)==0x20){
-      //measure time and timeout
-    }
+  //pull CS high to end spi
+  HAL_GPIO_WritePin(CC12_CSn_GPIO, CC12_CSn_PIN, GPIO_PIN_SET);
 }
-
-//free memory
-void handle_tx_memory(TxPayload_t *txBuffer){
-  if(txBuffer != NULL){
-    free(txBuffer->data);
-    free(txBuffer);
-  }
-}
-//get the error message type
-type get_tx_error(something){
-//complete implementation
-}
-//transmit message function
-
-
