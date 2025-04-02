b97a632d39fd799d258f16b19c72a81a0488ed70
Sydney Macdonald <macdonaldpsydney@gmail.com>
2025-04-01 18:08:35 -0700

    Add code structure for notification handling and transmitting camera

--- project/Core/Src/ttc.c
+++ project/Core/Src/ttc.c
@@ -10,24 +10,47 @@ void ttc_notifications(void *vpParameters) {
         received_notification = ulTaskNotifyTake(pdFALSE, 0);
 
         if (received_notification & CAMERA_READY) {
-            // TODO
+            // Call transmit function with pointer to camera data in flash
         }
 
         if (received_notification & CAMERA_ERROR) {
-        	// TODO
+        	// Tell ground station theres an error
+        }
+
+        if (received_notification & IDLE_WARNING) {
+        	// Can't transmit in IDLE mode
+        	// Do nothing
+        }
+
+        if (received_notification & LOW_POWER_WARNING) {
+        	// Can't take photos in LOW POWER mode
+        	// Tell ground station low power, no picture
+        	// Call transmit
         }
 
         if (received_notification & REQUEST_GPS) {
         	// Call interface function to read GPS
-			// Store GPS data
+        	int gps_result = 0; //gps_hl();
 
 			// Send task notification to the OBC
-			if(/*GPS data read correctly*/) {
+			if(gps_result & 0) {
 				ulTaskNotify(obc_notifications, GPS_READY, eSetValueWithOverwrite);
+				//transmit all data from flash
 			} else {
 				ulTaskNotify(obc_notifications, GPS_ERROR, eSetValueWithOverwrite);
 			}
         }
+
+        // if status change notif send to ground station
     }
 }
-// send to obc: REQUEST_CAMERA, GPS_READY, GPS_ERROR
+// If request for camera data Notify OBC that camera data is needed
+	// this should go in receive function:
+	// ulTaskNotify(obc_notifications, REQUEST_CAMERA, eSetValueWithOverwrite);
+
+void transmit_camera(/*pointer to camera data?, offset*/) {
+	// Call if acknowledgment of camera data + request for next bytes received
+	// call transmit with pointer to camera data + offset, type, total length
+}
+//pointer to payload, type (in slack), total length of camera data - give to transmit
+// if we need to retransmit need offset
