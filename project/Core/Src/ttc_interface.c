4854f6dc596975c5cc7cb050a991cbe50d31a4c0
avabirtwistle <148493409+avabirtwistle@users.noreply.github.com>
2025-04-03 18:46:25 -0700

    Update ttc_interface.c
    added to the code from my google colab.

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -4,9 +4,9 @@
 #include <string.h>
 #include<stdlib.h>
 
-#define MAX_PACKET_SIZE 256  //maximum packet size
-#define PACKET_HEADER_SIZE 4 //identifier, power, status, length
-#define PAYLOAD_SIZE 244     //max payload size
+#define MAX_PACKET_SIZE 32
+#define HEADER_SIZE 7
+#define MAX_PAYLOAD_PER_PACKET (MAX_PACKET_SIZE - HEADER_SIZE)
 #define SHARED_MEMORY_ADDRESS ((uint8_t*)0x80000000)
 
 typedef enum {
@@ -28,34 +28,42 @@ typedef enum {
     ACK_REC_ERROR = 0b00001111,
 } PayloadType;
 
-//very generic concept , will implement different ones to handle specific types
-void prepare_package(uint8_t *payload, uint8_t payloadLen) {
-    uint16_t totalLen = 1 + payloadLen + 2 + 4;
-    uint8_t *txBuffer = (uint8_t *)malloc(totalLen);
-    if (!txBuffer) return;
+void generatepacket(uint8_t type, uint8_t *payload, uint8_t payloadLen) {
+    // Send type
+    writeToTransmitBuffer(&type, 1);
 
-    uint16_t i = 0;
+    // Send payload
+    writeToTransmitBuffer(payload, payloadLen);
 
-    //type
-    txBuffer[i++] = type;
+    // Send offset (little-endian because of system memory layout)
+    writeToTransmitBuffer((uint8_t *)&offset, 2);
 
-    //payload
-    memcpy(&txBuffer[i], payload, payloadLen);
-    i += payloadLen;
+    // Send sequence number (little-endian)
+    writeToTransmitBuffer((uint8_t *)&sequenceNum, 4);
+}
 
-    //offset
-    txBuffer[i++] = offset & 0xFF;
-    txBuffer[i++] = (offset >> 8) & 0xFF;
+void packageAndSendChunks(uint8_t type, uint8_t *payload, uint16_t fullPayloadLen, uint32_t sequenceNum) {
+    uint16_t offset = 0;
 
-    //sequence Number 
-    txBuffer[i++] = sequenceNum & 0xFF;
-    txBuffer[i++] = (sequenceNum >> 8) & 0xFF;
-    txBuffer[i++] = (sequenceNum >> 16) & 0xFF;
-    txBuffer[i++] = (sequenceNum >> 24) & 0xFF;
+    while (offset < fullPayloadLen) {
+        // Determine the size of this chunk's payload
+        uint8_t chunkLen = (fullPayloadLen - offset > MAX_PAYLOAD_PER_PACKET)
+                           ? MAX_PAYLOAD_PER_PACKET
+                           : (fullPayloadLen - offset);
 
-    //now send that buffer
-   writeToTransmitBuffer(txBuffer, totalLen);
+        // Send type
+        writeToTransmitBuffer(&type, 1);
 
-    //cleanup
-    free(txBuffer);
+        // Send payload chunk
+        writeToTransmitBuffer(&payload[offset], chunkLen);
+
+        // Send offset (little-endian)
+        writeToTransmitBuffer((uint8_t *)&offset, 2);
+
+        // Send sequence number (little-endian)
+        writeToTransmitBuffer((uint8_t *)&sequenceNum, 4);
+
+        // Move to next chunk
+        offset += chunkLen;
+    }
 }
