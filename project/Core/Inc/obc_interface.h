e0a2c896dae3668f19dd5d2b82bf6cbcbea1ef38
Ethanium314 <ethanjamesdean@gmail.com>
2025-05-29 13:32:16 -0700

    Added prototypes for dummy functions

--- project/Core/Inc/obc_interface.h
+++ project/Core/Inc/obc_interface.h
@@ -8,4 +8,20 @@
 #define MAX_DATA_SIZE 100 // For telemetry, errors, warnings
 #define MAX_IMAGE_BUFFER_SIZE 100 // TODO: Temp
 
+void store_data(char* message, int status);
+
+void freeImageBuffer();
+
+int capture_snapshot();
+
+void save_image_to_flash();
+
+void Flash_Read_Data();
+
+void store_image();
+
+void read_bms();
+
+void read_sensors();
+
 #endif // OBC_INTERFACE_H
