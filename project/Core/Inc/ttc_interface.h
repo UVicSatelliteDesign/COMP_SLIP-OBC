7aeb28a040aea077fd4a59a9663a6a9759b99200
AniMB <animishmurthy@gmail.com>
2025-06-03 02:01:50 -0700

    Adds comments
    addee comments and fixed syntax issues in the files

--- project/Core/Inc/ttc_interface.h
+++ project/Core/Inc/ttc_interface.h
@@ -9,10 +9,10 @@
 
 
 
-typedef uint8_t FIFOsize[128];
-extern volatile bool dirtyBit;
-extern osMessageQueueId_t receivequeueHandle;
-extern osSemaphoreId_t myBinarySem01Handle;
+typedef uint8_t FIFOsize[128];  // new typedef used for defining FIFO size as mentioned in the CC1201 datasheet
+extern volatile bool dirtyBit;	// the bool dirty bit originally set to false
+extern osMessageQueueId_t receivequeueHandle; // the message queue used for receiving info from the trasnciever
+extern osSemaphoreId_t myBinarySem01Handle;   // used to protect the dirty bit. it is a sempahore
 
 
 
