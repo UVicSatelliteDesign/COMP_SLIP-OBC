d6514bf3829fca1d8829af5b9ed4d1cf2de822cd
Sydney Macdonald <macdonaldpsydney@gmail.com>
2025-06-15 14:09:54 -0700

    Add high level receive function to read incoming packet data

--- project/Core/Src/ttc.c
+++ project/Core/Src/ttc.c
@@ -47,6 +47,72 @@ void ttc_notifications(void *vpParameters) {
         }
     }
 }
-// If received packet is request for camera data, Notify OBC that camera data is needed
-	// this should go in receive function:
-	// ulTaskNotify(obc_notifications, REQUEST_CAMERA, eSetValueWithOverwrite);
+
+void receive() {
+	/*
+	 * Read from queue of bits:
+	 *
+	 * Length -> 1 byte
+	 * Payload type -> 1 byte
+	 * Data -> variable length (length - 2 [- 3])
+	 * Offset -> 3 bytes
+	 * Seq num -> 2 bytes
+	*/
+	uint8_t data_buffer[128];
+	osStatus_t status = osMessageQueueGet(receivequeueHandle, &data_buffer, NULL, 0U);   // wait for message
+	if (status != osOK) {
+		ulTaskNotify(obc_notifications, ERROR, eSetValueWithOverwrite);
+		return; // Error
+	}
+	uint8_t packet_length = data_buffer[0];
+	PayloadType packet_type = (PayloadType) data_buffer[1];
+
+	switch(packet_type) {
+		case PayloadType.PING:
+			// Acknowledge ping
+			generatepacket(PayloadType.PING, Null, 0);
+			break;
+		case PayloadType.NOMINAL:
+			ulTaskNotify(obc_notifications, REQUEST & NOMINAL, eSetValueWithOverwrite);
+			uint16_t seq_num = (data_buffer[2] << 8) | (data_buffer[3]);
+			generatepacket(PayloadType.ACK_REC_STATUS, seq_num, 2);
+			transmit();
+			break;
+		case PayloadType.LOW_POWER:
+			ulTaskNotify(obc_notifications, REQUEST & LOW_POWER, eSetValueWithOverwrite);
+			uint16_t seq_num = (data_buffer[2] << 8) | (data_buffer[3]);
+			generatepacket(PayloadType.ACK_REC_STATUS, seq_num, 2);
+			break;
+		case PayloadType.CAMERA_1_END:
+			// request for image, notify OBC
+			ulTaskNotify(obc_notifications, REQUEST & CAMERA, eSetValueWithOverwrite);
+			break;
+		case PayloadType.CAMERA_2_END:
+		// request for image, notify OBC
+			ulTaskNotify(obc_notifications, REQUEST & CAMERA, eSetValueWithOverwrite);
+			break;
+		case PayloadType.ACK_REC_CAMER:
+		// set as acknowledged
+		// Contains an offset
+			uint16_t acked_seq_num = (data_buffer[2] << 8) | (data_buffer[3]);
+			uint32_t acked_offset =
+					(data_buffer[4] << 16) |
+					(data_buffer[5] << 8) |
+					(data_buffer[6]);
+			uint16_t seq_num = (data_buffer[7] << 8) | (data_buffer[8]);
+			break;
+		case PayloadType.ACK_REC_TELEMETRY:
+		// set as acknowledged
+			uint16_t acked_seq_num = (data_buffer[2] << 8) | (data_buffer[3]);
+			break;
+		case PayloadType.ACK_REC_ERROR:
+		// set as acknowledged
+			uint16_t acked_seq_num = (data_buffer[2] << 8) | (data_buffer[3]);
+			break;
+		else:
+		// Error: should not be receiving other packet types
+			// Notify OBC of packet error
+			ulTaskNotify(obc_notifications, ERROR, eSetValueWithOverwrite);
+			break;
+	}
+}
