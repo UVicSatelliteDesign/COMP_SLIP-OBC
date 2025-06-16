0cfa926d642f7eacf27e1adb698f3fcc69f340e1
Sydney Macdonald <macdonaldpsydney@gmail.com>
2025-06-15 19:28:27 -0700

    Update notification values

--- project/Core/Src/ttc.c
+++ project/Core/Src/ttc.c
@@ -10,39 +10,45 @@ void ttc_notifications(void *vpParameters) {
         // Non-blocking check for notifications with a 0ms timeout
         received_notification = ulTaskNotifyTake(pdFALSE, 0);
 
-        if (received_notification & CAMERA_READY) {
+        if (received_notification & CAMERA & INFO) {
             // Call transmit function with pointer to camera data in flash
-        	generatepacket(/*CAMERA_1/2_MF*/, /*Pointer to cam data*/, /*Cam data length*/);
+        	generatepacket(PayloadType./*CAMERA_1/2_MF*/, /*Pointer to cam data*/, /*Cam data length*/);
+        	transmit();
         }
 
-        if (received_notification & CAMERA_ERROR) {
+        if (received_notification & CAMERA & ERROR) {
         	// Tell ground station there's an error
-        	generatepacket(ERROR_PER, /*Error message*/, /*Error msg length*/);
+        	generatepacket(PayloadType.ERROR_PER, CAMERA, 4);
+        	transmit();
         }
 
-        if (received_notification & IDLE_WARNING) {
+        if (received_notification & IDLE & WARNING) {
         	// Can't transmit in IDLE mode
         	// Do nothing
         }
 
-        if (received_notification & LOW_POWER_WARNING) {
+        if (received_notification & LOW_POWER & WARNING) {
         	// Can't take photos in LOW POWER mode
         	// Tell ground station low power, no picture
         	// Call transmit
-        	generatepacket(ERROR_LP, /*low power*/, /*msg len*/);
+        	generatepacket(PayloadType.ERROR_LP, Null, 0);
+        	transmit();
         }
 
-        if (received_notification & REQUEST_GPS) {
+        if (received_notification & REQUEST & GPS) {
         	// Call interface function to read GPS
         	int gps_result = gps_hl();
 
 			// Send task notification to the OBC
 			if(gps_result == 0) {
-				ulTaskNotify(obc_notifications, GPS_READY, eSetValueWithOverwrite);
+				xTaskNotify(obc_notifications, INFO & GPS, eSetValueWithOverwrite);
 				//transmit all data from flash
-				generatepacket(TELEMETRY, &TELEM_FLASH_MEM, TELEM_DATA_LEN);
+				generatepacket(PayloadType.TELEM, &TELEM_FLASH_MEM, TELEM_DATA_LEN);
+				transmit();
 			} else {
-				ulTaskNotify(obc_notifications, GPS_ERROR, eSetValueWithOverwrite);
+				xTaskNotify(obc_notifications, ERROR & GPS, eSetValueWithOverwrite);
+				generatepacket(PayloadType.ERROR_PER, GPS, 4);
+				transmit();
 			}
         }
     }
