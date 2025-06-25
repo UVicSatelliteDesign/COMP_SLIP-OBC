fde9cfc9d45517692b5e6b467fc054d0bf705304
hg <munroeparlee@gmail.com>
2025-06-24 19:50:29 -0700

    GPS low level written from pseudocode: polling receive code added; size, timeout, UART_HandleTypeDef huart3 variables defined

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -1,4 +1,13 @@
 #include "ttc_interface.h"
-void get_gps(){
-    //awaiting pseudocode: must be rewritten from SPI into UART
+#include "stm32h7xx_hal.h"
+
+uint8_t gps_rx_buffer[];
+//////////////// section variables need definition
+uint8_t NMEA_sentence_size; // NMEA sentence bit-length
+uint8_t interrupt_timeout_length; // time until the CPU unfreezes if data has not yet been received
+////////////////
+extern UART_HandleTypeDef huart3;
+
+void get_gps(){ // polling format
+    HAL_UART_Receive (&huart3, gps_rx_buffer, NMEA_sentence_size, interrupt_timeout_length);
 }
