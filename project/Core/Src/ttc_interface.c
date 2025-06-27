1b95c2d37e4ee30c02235745d1b5d118eb091c79
hg <munroeparlee@gmail.com>
2025-06-26 18:20:21 -0700

    updated code comments for clarity

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -1,13 +1,13 @@
 #include "ttc_interface.h"
 #include "stm32h7xx_hal.h"
 
-uint8_t gps_rx_buffer[];
+uint8_t gps_rx_buffer[]; // gps reception buffer
 //////////////// section variables need definition
-uint8_t NMEA_sentence_size; // NMEA sentence bit-length
-uint8_t interrupt_timeout_length; // time until the CPU unfreezes if data has not yet been received
+uint8_t NMEA_sentence_size; //  the length in bits of the gps data sentence
+uint8_t interrupt_timeout_length; //  the time until the CPU unfreezes if data has not yet been received
 ////////////////
-extern UART_HandleTypeDef huart3;
+extern UART_HandleTypeDef huart3; // this is the gps reception pin handler
 
-void get_gps(){ // polling format
+void get_gps(){ // this is formatted for polling
     HAL_UART_Receive (&huart3, gps_rx_buffer, NMEA_sentence_size, interrupt_timeout_length);
 }
