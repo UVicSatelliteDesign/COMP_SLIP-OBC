3df7ec7183d2ec010f3321b7a5e53603d1f78ef4
hg <munroeparlee@gmail.com>
2025-07-03 19:06:01 -0700

    declared GPS_FLASH_ADDRESS and called flash_write function in ttc_interface.c

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -6,6 +6,7 @@ uint8_t gps_rx_buffer[]; // gps reception buffer
 //////////////// section variables need definition
 uint8_t NMEA_sentence_size; //  the length in bits of the gps data sentence
 uint8_t interrupt_timeout_length; //  the time until the CPU unfreezes if data has not yet been received
+uint16_t GPS_FLASH_ADDRESS; // not actual address, change to real address
 ////////////////
 extern UART_HandleTypeDef huart3; // this is the gps reception pin handler
 
@@ -13,4 +14,5 @@ extern UART_HandleTypeDef huart3; // this is the gps reception pin handler
 ///////// get_gps() is waiting on a write to flash feature
 void get_gps(){ // this is formatted for polling
     HAL_UART_Receive (&huart3, gps_rx_buffer, NMEA_sentence_size, interrupt_timeout_length);
+    flash_write(*gps_rx_buffer, NMEA_sentence_size, GPS_FLASH_ADDRESS);
 }
