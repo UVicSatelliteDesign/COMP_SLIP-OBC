94de3aa37c1c786017d1b001010d7af5c8902f3e
hg <munroeparlee@gmail.com>
2025-07-03 18:40:37 -0700

    commented that a flash write call is needed

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -8,6 +8,8 @@ uint8_t interrupt_timeout_length; //  the time until the CPU unfreezes if data h
 ////////////////
 extern UART_HandleTypeDef huart3; // this is the gps reception pin handler
 
+
+///////// get_gps() is waiting on a write to flash feature
 void get_gps(){ // this is formatted for polling
     HAL_UART_Receive (&huart3, gps_rx_buffer, NMEA_sentence_size, interrupt_timeout_length);
 }
