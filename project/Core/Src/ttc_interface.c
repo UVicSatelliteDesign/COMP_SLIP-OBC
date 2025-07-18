9645c8d2ec6f34aceda7b182b1469a7a90b629fd
hg <munroeparlee@gmail.com>
2025-07-17 20:23:20 -0700

    updates the gps pin handler in ttc_interface.c from huart 3 to huart4

--- project/Core/Src/ttc_interface.c
+++ project/Core/Src/ttc_interface.c
@@ -9,7 +9,7 @@ uint8_t NMEA_sentence_size; //  the length in bits of the gps data sentence
 uint8_t interrupt_timeout_length; //  the time until the CPU unfreezes if data has not yet been received
 uint16_t GPS_FLASH_ADDRESS; // to be set before implementation
 ////////////////
-extern UART_HandleTypeDef huart3; // GPS UART handle definition
+extern UART_HandleTypeDef huart4; // GPS UART handle definition
 
 
 /*
@@ -22,7 +22,7 @@ Returns:
 
 */
 bool get_gps(){ // this is formatted for polling
-    HAL_UART_Receive (&huart3, gps_rx_buffer, NMEA_sentence_size, interrupt_timeout_length);
+    HAL_UART_Receive (&huart4, gps_rx_buffer, NMEA_sentence_size, interrupt_timeout_length);
     if(flash_write(*gps_rx_buffer, NMEA_sentence_size, GPS_FLASH_ADDRESS) == true){
         return true;
     }
