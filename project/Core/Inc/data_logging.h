eb7eab8e14d715ad40218729e337456c44a44c7a
hg <munroeparlee@gmail.com>
2025-07-21 23:46:02 -0700

    drafts log_data in data_logging.c, defines parameters and buffer memory address in data_logging.h

--- project/Core/Inc/data_logging.h
+++ project/Core/Inc/data_logging.h
@@ -4,3 +4,32 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "flash_interface.h"
+
+#define FLASH_MEMORY_SIZE_CAMERA     
+#define FLASH_MEMORY_SIZE_SENSORS    
+#define FLASH_MEMORY_SIZE_ALTIMETER  
+#define FLASH_MEMORY_SIZE_GPS        
+#define FLASH_MEMORY_SIZE_BATTERY    
+
+#define FLASH_ADDRESS_CAMERA           
+#define FLASH_ADDRESS_SENSORS         
+#define FLASH_ADDRESS_ALTIMETER     
+#define FLASH_ADDRESS_GPS                 
+#define FLASH_ADDRESS_BATTERY         
+
+#define DATA_LOGGING_BUFFER_MEMORY_ADDRESS
+
+#define CAMERA 0
+#define SENSORS 1
+#define ALTIMETER 2
+#define GPS 3
+#define BATTERY 4
+/*
+log_data: 
+    sends sensor data over serial to a receiver file (TODO: name receiver file once written)
+Parameters:
+    int data_specifier: specifies {CAMERA, SENSORS, ALTIMETER, GPS, BATTERY} i.e. {0, 1, 2, 3, 4}
+Return:
+    returns a true boolean for success and a false boolean for failure 
+*/
+bool log_data();
--- project/Core/Src/data_logging.c
+++ project/Core/Src/data_logging.c
@@ -4,3 +4,41 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "flash_interface.h"
+#include "data_logging.h"
+
+bool log_data(int data_specifier){
+    switch(data_specifier) {
+        case CAMERA:
+            if(flash_read(DATA_LOGGING_BUFFER_MEMORY_ADDRESS, FLASH_MEMORY_SIZE_CAMERA, FLASH_ADDRESS_CAMERA)){
+                //print buffer to serial
+                return true;
+            }
+            return false;
+        case SENSORS:
+            if(flash_read(DATA_LOGGING_BUFFER_MEMORY_ADDRESS, FLASH_MEMORY_SIZE_SENSORS, FLASH_ADDRESS_SENSORS)){
+                //print buffer to serial
+                return true;
+            }
+            return false;
+        case ALTIMETER:
+            if(flash_read(DATA_LOGGING_BUFFER_MEMORY_ADDRESS, FLASH_MEMORY_SIZE_ALTIMETER, FLASH_ADDRESS_ALTIMETER)){
+                //print buffer to serial
+                return true;
+            }
+            return false;
+        case GPS:
+            if(flash_read(DATA_LOGGING_BUFFER_MEMORY_ADDRESS, FLASH_MEMORY_SIZE_GPS, FLASH_ADDRESS_GPS)){
+                //print buffer to serial
+                return true;
+            }
+            return false;
+        case BATTERY:
+            if(flash_read(DATA_LOGGING_BUFFER_MEMORY_ADDRESS, FLASH_MEMORY_SIZE_BATTERY, FLASH_ADDRESS_BATTERY)){
+                //print buffer to serial
+                return true;
+            }
+            return false;
+        default:
+            return false;
+        }
+}
